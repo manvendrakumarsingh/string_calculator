3bdc4c3d9f159eb9ef9b91d3c880387efed8d8f2
manvendrakumarsingh <manu123singh@gmail.com>
2023-05-07 19:09:51 +0530

    Write test case for single digit

--- spec/requests/calculators_spec.rb
+++ spec/requests/calculators_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Calculators", type: :request do
   describe "POST /create" do
+
+    context "with single number parameters" do
+      let(:string_number_params) { { string_numbers: "1"} }
+      it "add number" do
+        post "/calculators", params: string_number_params
+        expect JSON(response.body)["response"] == 1
+      end
+
+      it "returns a success response" do
+        post "/calculators", params: string_number_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
     context "with valid parameters" do
       let(:string_number_params) { { string_numbers: "1,2"} }
       it "add number" do
